c1aa8efd55c49e35546d6b19652b74a3416e317e
HJA Bird <h.bird.1@research.gla.ac.uk>
2020-04-08 00:20:58 +0100

    Refactor for perf.

--- src/VortFunc.c
+++ src/VortFunc.c
@@ -10,14 +10,14 @@ and measurement point x divided by regularisation distance sigma then
 omega(x) = sum( zeta(rho_i) * alpha_i )
 
 These regularisation function are given by zeta, but exclude the 4*pi
-part - that constant is in the evaluation bits. 
+part - that constant is in the evaluation bits.
 
 The velocity includes a function g(rho) defined by
 zeta(rho) = 1/rho^2 * dg/drho
 again excluding the 4 pi bit.
 
-For the particle strenght exchange schmeme another funcion eta(rho) is 
-required. 
+For the particle strenght exchange schmeme another funcion eta(rho) is
+required.
 eta(rho) = -1/rho * (dzeta/drho)
 
 Copyright(c) 2018-2020 HJA Bird
@@ -46,52 +46,55 @@ SOFTWARE.
 #include <stdio.h>
 #include <string.h>
 
-static float warn_bad_eta_fn(float rho){
+#define SQRTF_2_OVER_PI 0.7978845608028654f
+#define RECIP_SQRTF_2 0.7071067811865475f
+
+static float warn_bad_eta_fn(float rho) {
 	static int warned = 0;
 	assert(0 && "Vortex regularisation function had no viscous method!");
-	if(warned == 0){
+	if (warned == 0) {
 		fprintf(stderr, "Tried to calculated perform viscous calculations "
 			"with inapproriate vortex regularisation function!\n"
 			"Modelling as invicid.\n\n");
 		warned = 1;
 	}
-	return 0;
+	return 0.f;
 }
 
 static float g_singular_3D(float rho) {
-	return 1;
+	return 1.f;
 }
 
 static float zeta_singular_3D(float rho) {
-	return 0;
+	return 0.f;
 }
 
 static void combined_singular_3D(float rho, float* g, float* zeta) {
-	*g = 1;
-	*zeta = 0;
+	*g = 1.f;
+	*zeta = 0.f;
 	return;
 }
 
 static float g_singular_2D(float rho) {
-	return 1;
+	return 1.f;
 }
 
 static float g_winckel_3D(float rho) {
 	float a, b, c, d;
 	assert(rho >= 0 && "Rho should not be -ve");
-	a = rho * rho + 2.5f;
-	b = a * rho * rho * rho;
-	c = rho * rho + 1;
-	d = (b / powf(c, 2.5f));
+	a = (rho * rho) + 2.5f;
+	b = a * rho * (rho * rho);
+	c = (rho * rho) + 1.f;
+	d = b * powf(c, -2.5f);
 	return d;
 }
 
 static float zeta_winckel_3D(float rho) {
 	float a, b, c;
 	assert(rho >= 0 && "Rho should not be -ve");
-	a = rho * rho + 1;
-	b = powf(a, 3.5f);
-	c = 7.5f / b;
+	a = rho * rho + 1.f;
+	b = powf(a, -3.5f);
+	c = 7.5f * b;
 	return c;
 }
 
@@ -99,7 +102,7 @@ static float eta_winckel_3D(float rho) {
 	float a, b, c;
 	assert(rho >= 0 && "Rho should not be -ve");
 	a = 52.5f;
-	b = rho * rho + 1;
+	b = rho * rho + 1.f;
 	c = powf(b, -4.5f);
 	return a * c;
 }
@@ -113,9 +116,8 @@ static void combined_winckel_3D(float rho, float* g, float* zeta) {
 
 static float g_winckel_2D(float rho) {
 	float num, denom;
-	num = rho * rho * (rho * rho + 2.f);
-	denom = rho * rho + 1.f;
-	denom = denom * denom;
+	num = (rho * rho) * (rho * rho) + (rho * rho) * 2.f;
+	denom = (rho * rho) * (rho * rho) + 2.f * (rho * rho) + 1.f;
 	return num / denom;
 }
 
@@ -134,7 +136,7 @@ static float g_planetary_3D(float rho) {
 	return rho < 1.f ? rho * rho * rho : 1.f;
 }
 
-static float zeta_planetary_3D(float rho){
+static float zeta_planetary_3D(float rho) {
 	assert(rho >= 0 && "Rho should not be -ve");
 	return rho < 1.f ? 3.f : 0.f;
 }
@@ -151,29 +153,30 @@ static float g_planetary_2D(float rho) {
 	return rho < 1.f ? rho * rho : 1.f;
 }
 
-static float g_gaussian_3D(float rho){
+static float g_gaussian_3D(float rho) {
 	/* = 1 to 8sf for rho ~>6. Taylor expansion otherwise */
 	assert(rho >= 0 && "Rho should not be -ve");
 	const float pi = 3.14159265359f;
 	float ret;
-	if(rho > 6.f){
+	if (rho > 6.f) {
 		ret = 1.f;
-	} else {
+	}
+	else {
 		/* Approximate erf using Abramowitz and Stegan 1.7.26 */
 		float a1 = 0.3480242f, a2 = -0.0958798f, a3 = 0.7478556f, p = 0.47047f;
-		float rho_sr2 = rho / sqrtf(2);
+		float rho_sr2 = rho * RECIP_SQRTF_2;
 		float t = 1.f / (1.f + p * rho_sr2);
-		float erf = 1.f-t * (a1 + t * (a2 + t * a3)) * expf(-rho_sr2 * rho_sr2);
-		float term2 = rho * sqrtf(2.f / pi) * expf(-rho_sr2 * rho_sr2);
+		float erf = 1.f - t * (a1 + t * (a2 + t * a3)) * expf(-rho_sr2 * rho_sr2);
+		float term2 = rho * SQRTF_2_OVER_PI * expf(-rho_sr2 * rho_sr2);
 		ret = erf - term2;
 	}
 	return ret;
 }
 
-static float zeta_gaussian_3D(float rho){
+static float zeta_gaussian_3D(float rho) {
 	assert(rho >= 0 && "Rho should not be -ve");
 	const float pi = 3.14159265359f;
-	return sqrtf(2.f / pi) * expf(-rho * rho / 2.f);
+	return SQRTF_2_OVER_PI * expf(-rho * rho / 2.f);
 }
 
 static void combined_gaussian_3D(float rho, float* g, float* zeta) {
@@ -185,12 +188,12 @@ static void combined_gaussian_3D(float rho, float* g, float* zeta) {
 
 static float g_gaussian_2D(float rho) {
 	assert(rho >= 0 && "Rho should not be -ve");
-	return 1 - expf(-rho * rho / 2);
+	return 1.f - expf(-rho * rho * 0.5f);
 }
 
 static float zeta_gaussian_2D(float rho) {
 	assert(rho >= 0 && "Rho should not be -ve");
-	return expf(-rho * rho / 2.f);
+	return expf(-rho * rho * 0.5f);
 }
 
 CVTX_EXPORT const cvtx_VortFunc cvtx_VortFunc_singular(void)
@@ -232,12 +235,12 @@ CVTX_EXPORT const cvtx_VortFunc cvtx_VortFunc_planetary(void)
 	return ret;
 }
 
-CVTX_EXPORT const cvtx_VortFunc cvtx_VortFunc_gaussian(void){
+CVTX_EXPORT const cvtx_VortFunc cvtx_VortFunc_gaussian(void) {
 	cvtx_VortFunc ret;
 	ret.g_3D = &g_gaussian_3D;
 	ret.g_2D = &g_gaussian_2D;
 	ret.zeta_3D = &zeta_gaussian_3D;
-	ret.eta_3D = &zeta_gaussian_3D; 
+	ret.eta_3D = &zeta_gaussian_3D;
 	ret.eta_2D = &zeta_gaussian_2D;
 	/* See Winckelmans et al., C. R. Physique 6 (2005), around eq (28) */
 	ret.combined_3D = &combined_gaussian_3D;
